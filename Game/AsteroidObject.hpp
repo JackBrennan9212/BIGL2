1cb064aa0c7c63a8e9c18c9f0b90561f0e59091b
JackBrennan9212 <32307467+JackBrennan9212@users.noreply.github.com>
2018-04-22 19:34:01 -0400

    Update AsteroidObject.hpp

--- Game/AsteroidObject.hpp
+++ Game/AsteroidObject.hpp
@@ -1,28 +1,45 @@
 
+//XCode header
 #ifndef AsteroidObject_hpp
 #define AsteroidObject_hpp
 
 #include "Game.hpp"
 
+//header class for asteroid object
+
 class AsteroidObject{
 public:
+    //declare variables as public
+    
+    //asteroid object parameters
     AsteroidObject(const char* texturesheet, int x, int y);
+    //asteroid object
     ~AsteroidObject();
     
+    //update method
     void update();
+    //render method
     void render();
+    //xval method
     int xval();
+    //yval method
     int yval();
     
     
-    
 private:
+    //declare variables as private
+    
+    //creates x position variable
     int xpos;
+    //creates y position variable
     int ypos;
     
+    //points SDL Texture to an object
     SDL_Texture* objTexture;
+    //declares the SDL Rectangle
     SDL_Rect srcRect, destRect;
     
 };
 
+//ends the asteroid object header class
 #endif /* AsteroidObject_hpp */
