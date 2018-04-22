1f8279214b9feafbdbceae330dc84a5ca0ce3746
JackBrennan9212 <32307467+JackBrennan9212@users.noreply.github.com>
2018-04-22 19:58:12 -0400

    Update PlayerObject.hpp

--- Game/PlayerObject.hpp
+++ Game/PlayerObject.hpp
@@ -1,23 +1,39 @@
 #include "Game.hpp"
 
+//header class for player object
 class PlayerObject{
 public:
+    //declare variables as public
+
+    //defines player object
     PlayerObject(const char* texturesheet, int x, int y);
+    //player object
     ~PlayerObject();
     
+    //update method
     void update();
+    //render method
     void render();
     
+    //move left method
     void moveLeft();
+    //move right method
     void moveRight();
     
+    //xval method
     int xval();
     
 private:
+    //declare variables as private
+    
+    //creates x position variable
     int xpos;
+    //creates y position variable
     int ypos;
     
+    //points SDL Texture to an object
     SDL_Texture* objTexture;
+    //declares the SDL Rectangle
     SDL_Rect srcRect, destRect;
     
 };
