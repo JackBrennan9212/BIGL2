747222fbea2f26e34f3c366a530b78153396e43b
JackBrennan9212 <32307467+JackBrennan9212@users.noreply.github.com>
2018-04-22 20:06:41 -0400

    Update Game.hpp

--- Game/Game.hpp
+++ Game/Game.hpp
@@ -5,26 +5,36 @@
 #include "SDL2/SDL.h"
 #include "SDL2_image/SDL_image.h"
 
-
+//header class for Game class
 
 class Game{
     
 public:
+    //declares variables as public
+    
+    //Game method and parameters
     Game();
+    //Game
     ~Game();
     
-   
-    
+    //initializing method and parameters
     void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+    //handle events method
     void handleEvents();
+    //update method
     void update();
+    //render method
     void render();
+    //clean up method
     void clean();
+    //SDL points to renderer
     static SDL_Renderer *renderer;
     
+    //running method and return variable
     bool running(){return isRunning;}
-    
+    //check collision method
     bool checkCollision();
+    //endede here
     bool checkBigCollision();
     
  
